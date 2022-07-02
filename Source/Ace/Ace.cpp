529baf16de85f125513def414d0feb4b82c605dc
felocsss <vlad.shipilov.felocs@gmail.com>
2022-07-02 16:39:38 +0300

    Initial commit

---
+++ Source/Ace/Ace.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Ace.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Ace, "Ace" );
---
+++ Source/Ace/AceGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AceGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACE_API AAceGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
